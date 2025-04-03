bcb20d77842aa8b1c03deb34afe29fc024b1ad92
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-04-03 14:49:03 +0000

    I added some logic

--- app/src/main/java/com/example/imad5112a1_2024/MainActivity.kt
+++ app/src/main/java/com/example/imad5112a1_2024/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.imad5112a1_2024
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        val button = findViewById<Button>(R.id.button)
+
+
+        val textView = findViewById<TextView>(R.id.textView)
+        val editTextText = findViewById<EditText>(R.id.editTextText)
+
+
+        button?.setOnClickListener {
+            var greeting: String = ""
+
+
+            greeting = "Welcome, ${editTextText.text}"
+            textView.text = greeting
+
+
+
+
+        }
     }
 }
