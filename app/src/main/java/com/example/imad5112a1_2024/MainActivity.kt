b911e7f11367ed67a6dc56674b7f1f10219f645b
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-04-03 15:09:57 +0000

    Added logic

--- app/src/main/java/com/example/imad5112a1_2024/MainActivity.kt
+++ app/src/main/java/com/example/imad5112a1_2024/MainActivity.kt
@@ -21,11 +21,46 @@ class MainActivity : AppCompatActivity() {
 
 
         button?.setOnClickListener {
-            var greeting: String = ""
+
+            val numberValue = editTextText.text.toString().toIntOrNull()
+
+            var result: String = ""
+
+            if(numberValue == null){
+                result = "Please enter a number."
+            } else {
+                if(numberValue < 10){
+                    result = "Please enter a number higher than 10"
+                } else if(10 < numberValue && numberValue <= 15) {
+                    result = "Anne Frank – Diarist during the Holocaust (died at 15)"
+                } else if(15 < numberValue && numberValue <= 20) {
+                    result = "Joan of Arc – French heroine and martyr (died at 19)"
+                } else if(20 < numberValue && numberValue <= 25) {
+                    result = "Tupac Shakur – Rapper and activist (died at 25)"
+                } else if(25 < numberValue && numberValue <= 30) {
+                    result = "Heath Ledger – Actor (died at 28)"
+                } else if(30 < numberValue && numberValue <= 35) {
+                    result = "Bruce Lee – Martial artist and actor (died at 32)"
+                } else if(35 < numberValue && numberValue <= 40) {
+                    result = "Amelia Earhart – Aviator and explorer (disappeared at 39)"
+                } else if(40 < numberValue && numberValue <= 45) {
+                    result = "Chadwick Boseman – Actor (died at 43)"
+                } else if(45 < numberValue && numberValue <= 50) {
+                    result = "George Orwell – Author of 1984 and Animal Farm (died at 46)"
+                } else if(50 < numberValue && numberValue <= 55) {
+                    result = "George Michael – Pop legend and former Wham! member (died at 53)"
+                } else if(55 < numberValue && numberValue <= 60) {
+                    result = "Steve Jobs – Co-founder of Apple (died at 56)"
+                } else if(60 < numberValue && numberValue <= 65) {
+                    result = "Anthony Bourdain – Renowned chef, author, and travel documentarian (died at 61)"
+                } else if(65 < numberValue && numberValue <= 70) {
+                    result = "Leonardo da Vinci – Inventor and artist (died at 67)"
+                }
+            }
 
 
-            greeting = "Welcome, ${editTextText.text}"
-            textView.text = greeting
+
+            textView.text = result
 
 
 
